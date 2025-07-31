76370fec726dca0bc9bb68f103842327daa566ab
AmongRyzh <sasharyzh@icloud.com>
2025-07-31 12:08:57 +0300

    отсортировал по папочкам

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -22,3 +22,21 @@ func _on_settings_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+
+func _on_music_slider_value_changed(value):
+	print(value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value))
+
+
+func _on_sfx_slider_value_changed(value):
+	print(value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(value))
+
+
+func _on_fullscreen_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
