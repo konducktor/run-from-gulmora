979fb8d41d56635b449d83c8c3e6f256dcf99cfd
PopDash <78530115+kondukctor@users.noreply.github.com>
2025-08-03 19:40:17 +0300

    фиксы №3

--- levels/level_generator.gd
+++ levels/level_generator.gd
@@ -105,16 +105,13 @@ func generate_next_level(custom_level: PackedScene = null) -> Level:
 	
 	if custom_level:
 		next_level = custom_level
-		print('custom_level')
 	elif (
 		GlobalValues.is_generating_letter_levels and
 		(current_level_index % LEVELS_IN_LAYER) == letter_level_index_in_layer
 	):
-		print('current_letter_level')
 		next_level = current_letter_level
 		layer_levels_generated += 1
 	else:
-		print('pick_level_from_level_array(level_array)')
 		next_level = pick_level_from_level_array(level_array)
 		layer_levels_generated += 1
 	
