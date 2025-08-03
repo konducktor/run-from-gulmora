1eac41bdad31fa76f9d2e2920fd463cb24ca0a63
PopDash <78530115+kondukctor@users.noreply.github.com>
2025-08-03 17:47:39 +0300

    Фиксы генерации уровней №1

--- global/gameplay.gd
+++ global/gameplay.gd
@@ -4,4 +4,4 @@ extends Node2D
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed('restart'):
 			if get_tree():
-				get_tree().reload_current_scene()
+				get_tree().call_deferred('reload_current_scene')
